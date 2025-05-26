b810852fa1df3025300ac88fefaffe38bff65480
Secozzi <folke.steen85@gmail.com>
2025-05-26 09:46:31 +0200

    small code cleanup

--- composeApp/src/commonMain/kotlin/xyz/secozzi/jellyfinmanager/ui/jellyfin/search/JellyfinSearchScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/xyz/secozzi/jellyfinmanager/ui/jellyfin/search/JellyfinSearchScreenViewModel.kt
@@ -1,17 +1,15 @@
 package xyz.secozzi.jellyfinmanager.ui.jellyfin.search
 
 import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import xyz.secozzi.jellyfinmanager.domain.jellyfin.JellyfinRepository
 import xyz.secozzi.jellyfinmanager.domain.jellyfin.models.JellyfinSearchProvider
 import xyz.secozzi.jellyfinmanager.domain.jellyfin.models.JellyfinSearchResult
 import xyz.secozzi.jellyfinmanager.presentation.utils.StateViewModel
-import xyz.secozzi.jellyfinmanager.presentation.utils.UIState
+import xyz.secozzi.jellyfinmanager.presentation.utils.executeCatching
 
 class JellyfinSearchScreenViewModel(
     savedStateHandle: SavedStateHandle,
@@ -25,19 +23,13 @@ class JellyfinSearchScreenViewModel(
     val items = _items.asStateFlow()
 
     fun search(query: String) {
-        mutableState.update { _ -> UIState.Loading }
-        viewModelScope.launch {
-            try {
-                val searchResult = jellyfinRepository.searchSeries(
-                    id = searchRoute.itemId,
-                    searchProvider = searchRoute.searchProvider.providerName,
-                    searchQuery = query,
-                )
-                _items.update { _ -> searchResult }
-                mutableState.update { _ -> UIState.Success }
-            } catch (e: Exception) {
-                mutableState.update { _ -> UIState.Error(e) }
-            }
+        executeCatching {
+            val searchResult = jellyfinRepository.searchSeries(
+                id = searchRoute.itemId,
+                searchProvider = searchRoute.searchProvider.providerName,
+                searchQuery = query,
+            )
+            _items.update { _ -> searchResult }
         }
     }
 
